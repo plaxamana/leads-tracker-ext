6ad7f07a0425a29b531050cd0ed665c6ead7fe52
Philip Laxamana <philip.laxamana@hotmail.com>
2021-09-06 10:07:56 -0400

    working version of leads tracker

---
+++ README.md
@@ -0,0 +1,16 @@
+# Chrome Leads Tracker Extension
+
+Building a simple chrome extension to track and collect leads.  
+
+## How to install
+
+Download this folder from github as a zip, and extract it.  Open your chrome browser to `chrome://extensions`
+
+```
+Enable developer mode
+Load unpacked
+Select 'leads-tracker-ext'
+Enable extension
+```
+
+Now the extension should be available to you at the top right corner of your chrome window
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
 
 // get leads from localStorage
 let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
@@ -16,6 +17,12 @@ inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = "";
   localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  renderLeads();
+});
+
+deleteBtn.addEventListener("dblclick", (e) => {
+  localStorage.clear();
+  myLeads = [];
   renderLeads();
 });
 
