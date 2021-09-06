4e1254dedc941acc601d61c4cf2c2d5073339e3d
Philip Laxamana <philip.laxamana@hotmail.com>
2021-09-06 09:42:38 -0400

    fetched leads from localStorage

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
-let myLeads = []
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    renderLeads()
-})
+// get leads from localStorage
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+// check if leadsFromLocalStorage exists
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  renderLeads();
+}
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  renderLeads();
+});
 
 function renderLeads() {
-    let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
-        listItems += `
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `
             <li>
                 <a target='_blank' href='${myLeads[i]}'>
                     ${myLeads[i]}
                 </a>
             </li>
-        `
-    }
-    ulEl.innerHTML = listItems  
+        `;
+  }
+  ulEl.innerHTML = listItems;
 }
